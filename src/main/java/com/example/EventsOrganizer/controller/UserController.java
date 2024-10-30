cda962f2ad8230b55cf691c630aac7310d4a1ad0
Ilya Kuhtin <voodoosproject@gmail.com>
2024-10-30 11:32:19 +0700

    Добавлены пустые методы в UserController

---
+++ src/main/java/com/example/EventsOrganizer/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.EventsOrganizer.controller;
+
+
+import com.example.EventsOrganizer.model.dto.UserDto;
+import com.example.EventsOrganizer.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/")
+    public List<UserDto> getUsers() {
+       return null;
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto getUser(@PathVariable String userId) {
+        return null;
+    }
+
+    @GetMapping("/{userId}/sub/events")
+    public EventDto getEventsByUser(@PathVariable String userId) {
+        return null;
+    }
+
+    @GetMapping("/{userId}/sub/events")
+    public ClubDto getClubsByUser(@PathVariable String userId) {
+        return null;
+    }
+
+
+
+//    @PostMapping(value = "/save")
+//    public UserDto addUser(@RequestBody UserDto userDto) {
+//        return userService.saveUser(userDto);
+//    }
+
+}
